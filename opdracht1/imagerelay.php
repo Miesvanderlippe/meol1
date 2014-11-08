417130469394bc3e7ef86521f253ab008854c345
Miesvanderlippe <miesvanderlippe@gmail.com>
2014-11-08 23:52:13 +0100

    Opdracht 1 | Image relay comment
    Added some commentary to the imagerelay script

--- opdracht1/imagerelay.php
+++ opdracht1/imagerelay.php
@@ -1,12 +1,20 @@
 <?php
 
-$apikey = "b20a23e4857a0c68";
-$apiurl = "http://api.wunderground.com/api/";
+/*
+	Little script to add private bits to the url and relay the image found there.
+	This results in never displaying your API key to the public.
 
-$urlPrefix = $apiurl . $apikey;
-$urlSuffix = urldecode($_GET['url']);
+	This script is incomplete and just a proof of concept that you can indeed hide private keys.
+	It needs checks for everything and access restrictions
+*/
+
+$apikey		 = "b20a23e4857a0c68";
+$apiurl		 = "http://api.wunderground.com/api/";
+
+$urlPrefix	 = $apiurl . $apikey;
+$urlSuffix	 = urldecode($_GET['url']);
 
 $remoteImage = $urlPrefix . $urlSuffix;
-$imginfo = getimagesize($remoteImage);
+$imginfo	 = getimagesize($remoteImage);
 header("Content-type: " . $imginfo['mime'] );
 readfile($remoteImage);

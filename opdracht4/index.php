c35bcd35a74abc1911e12667f3c46a39d4d41d72
Miesvanderlippe <miesvanderlippe@gmail.com>
2014-11-22 12:25:56 +0100

    Opdracht 4 | Start of DBModel

--- opdracht4/index.php
+++ opdracht4/index.php
@@ -2,6 +2,31 @@
 
 require_once('../Slim/Slim.php');
 
+class DB extends PDO {
+
+	protected $dbname	 = 'db71989';
+	protected $dbuser	 = 'root';
+	protected $dbpw		 = 'usbw';
+	protected $dbport	 = '3307';
+	protected $dbserver  = 'localhost';
+
+	public function __construct($options = Null){
+		
+		if(!isset($options)) {
+
+			$options = array(
+		    
+		    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+			);
+		}
+
+		parent::__construct('mysql:host='. $this->dbserver . ';dbname=' . $this->dbname . ';port=' . $this->dbport, $this->dbuser, $this->dbpw, $options);
+	}
+}
+
+$db = new DB();
+
 \Slim\Slim::registerAutoloader();
 
 $slim = new \Slim\Slim();

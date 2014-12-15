072ca6de09efe9e433a9d356f32b3ffeb5cc7d1d
miesvanderlippe <miesvanderlippe@gmail.com>
2014-12-15 11:19:58 +0100

    Opdracht 5 | Copied API

---
+++ opdracht5/index.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once('../Slim/Slim.php');
+require_once('database.php');
+
+\Slim\Slim::registerAutoloader();
+
+$slim 	 = new \Slim\Slim();
+$db 	 = new DB();
+
+$slim->get('/dieren', function()
+	use($db){
+
+	$dieren 	 = $db->GetAllAnimals();
+	
+	print(json_encode($dieren));
+});
+
+$slim->post('/dieren', function()
+	use($slim, $db){
+		
+		$name		 = $app->request()->post('name');
+        $kind		 = $app->request()->post('kind');
+        $yearOfBirth = $app->request()->post('yearOfBirth');
+        $ownerID	 = $app->request()->post('ownerID');
+
+		$result		 = $db->AddAnimal($name, $kind, $yearOfBirth, $ownerID);
+
+		print(json_encode(array('result'=>$result)));
+});
+
+$slim->get('/dieren/:id', function($id)
+	use($db){
+
+	$dieren 	 = $db->GetAnimal($id);
+	
+	print(json_encode($dieren));
+});
+
+$slim->get('/dieren/:id/eigenaar', function($id)
+	use($db){
+
+	$dieren 	 = $db->GetOwnerByPet($id);
+	
+	print(json_encode($dieren));
+});
+
+$slim->get('/eigenaars', function()
+	use($db){
+
+	$eigenaars 	 = $db->GetAllOwners();
+	
+	print(json_encode($eigenaars));
+});
+
+$slim->post('/owners', function()
+	use($slim, $db){
+		
+		$firstName	 = $app->request()->post('firstName');
+        $affix		 = $app->request()->post('affix');
+        $lastName	 = $app->request()->post('lastName');
+        $city		 = $app->request()->post('city');
+
+		$result		 = $db->AddOwner($firstName, $affix, $lastName, $city);
+
+		print(json_encode(array('result'=>$result)));
+});
+
+$slim->get('/eigenaars/:id', function($id)
+	use($db){
+
+	$eigenaar 	 = $db->GetOwner($id);
+	
+	print(json_encode($eigenaar));
+});
+
+$slim->get('/eigenaars/:id/dieren', function($id)
+	use($db){
+
+	$eigenaar 	 = $db->GetAnimalsByOwner($id);
+	
+	print(json_encode($eigenaar));
+});
+
+$slim->run();

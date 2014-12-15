db562f6d88782d5c18fd7e5d24fa0338c1bad80f
miesvanderlippe <miesvanderlippe@gmail.com>
2014-12-15 11:36:32 +0100

    Opdracht 5 | CryptClass (rand string)

---
+++ opdracht5/classes/crypt.php
@@ -0,0 +1,18 @@
+<?php
+
+class Crypt {
+	
+	public static function RandomString($length = 16){
+
+		$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	    $charactersLength = strlen($characters);
+	    
+	    $randomString = '';
+	    
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	    }
+	    
+	    return $randomString;
+	}
+}
